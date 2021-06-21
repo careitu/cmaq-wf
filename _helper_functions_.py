599d7cadeec4aeaf0811ef8dfbf49ab71f41b2df
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-06-21 20:48:21 +0300

    fix cmaq success string

--- _helper_functions_.py
+++ _helper_functions_.py
@@ -192,4 +192,4 @@ def run_script_bcon(script, dom, str_date):
 
 
 def run_script_cctm(script, dom, str_date):
-    run_script(script, dom, str_date, 'cctm', b'XXXXX')
+    run_script(script, dom, str_date, 'cctm', b'CMAQ TIMING REPORT')
