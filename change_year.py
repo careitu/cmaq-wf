31b588eabf89999c8274bace9431cb3897056470
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-07-15 04:09:25 +0300

    Update change_year.py
    improve search feature

--- change_year.py
+++ change_year.py
@@ -7,11 +7,21 @@ Change year in CMAQ netcdf files
 ~~~~~~~
 Python script to change year in CMAQ netcdf files
 """
-
+import os
 from netCDF4 import Dataset
 from settings import setting as s
 
 proj = s.get_active_proj()
+
+
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            yield from getListOfFiles(fullPath)
+        else:
+            yield fullPath
 
 
 def change_year(nc_file, new_year, var_name='TFLAG'):
@@ -21,7 +31,8 @@ def change_year(nc_file, new_year, var_name='TFLAG'):
     for i in range(s[0]):
         for j in range(s[1]):
             val = v[i][j][0]
-            nco[var_name][i, j, 0] = val + (new_year - val // 1000) * 1000
+            # nco[var_name][i, j, 0] = val + (new_year - val // 1000) * 1000
+            nco[var_name][i, j, 0] = new_year * 1000 + val % 1000
     nco.close()
 
 
@@ -34,8 +45,10 @@ def _parse_args_():
     EPILOG = 'Example of use:\n' + \
              ' %(prog)s -y 2025 file_name.nc\n'
     p = _create_argparser_(DESCRIPTION, EPILOG)
-    p.add_argument('files', metavar='FILES', type=str, nargs='+',
-                   help='File names')
+    p.add_argument('-d', '--dir', type=str, default=os.getcwd(),
+                   help='Directory to search. Default is current directory')
+    p.add_argument('file_pattern', metavar='FILE_PATTERN', type=str, nargs='+',
+                   help='File patterns to search')
     p.add_argument('-y', '--year', type=int, default=2025,
                    help='default is 2025.')
     return p.parse_args()
@@ -48,6 +61,31 @@ new_year = 2025
 
 if __name__ == "__main__":
     a = _parse_args_()
+    if a.dir is None:
+        d = os.getcwd()
+    else:
+        if a.dir in proj.path.__dict__.keys():
+            d = proj.path.__dict__[a.dir]
+        else:
+            d = a.dir
 
-    for f in a.files:
-        change_year(f, a.year)
+    print('\nDirectory is "{}"'.format(d))
+    user_input = input('Confirm? [Y/N] ')
+
+    if user_input.lower() in ('y', 'yes'):
+        for f in getListOfFiles(d):
+            for fp in a.file_pattern:
+                if fp.lower() in f.lower():
+                    print(f)
+
+
+
+    # for f in a.files:
+    #     if f in proj.path.__dict__.keys():
+    #         f = proj.path.__dict__[f]
+    #         files = getListOfFiles(f)
+    #         for k in files:
+    #             print(k)
+    #     else:
+    #         print(f)
+        # change_year(f, a.year)
