47d996bfa808af8363cf26b37a318873a3dfa07d
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-12-27 08:46:46 +0300

    Update performance.py

--- performance.py
+++ performance.py
@@ -4,46 +4,95 @@
 
 
 """
-Calculşate performance for CMAQ vs. Observations
+Calculate performance for CMAQ vs. Observations
 ~~~~~~~~
 """
+import numpy as np
+import xarray as xr
 
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from matplotlib.colors import LinearSegmentedColormap as lscmap
 
-from settings import setting as _set
-
-matplotlib.use('Agg')
+from _helper_functions_ import max_mult
 
 from settings import setting as _set
+
 from DAL import *
 
-proj = _set.get_proj_by_name('cityair')
-dom_names=['aegean', 'mediterranean',
-           'south_central_anatolia',
-           'central_blacksea']
-pol_names = list(proj.pols.keys())
-pols = {p: proj.pols[p] for p in pol_names}
-pol_names = [p.name for p in pols.values()]
+import sklearn.metrics as mp
+from math import sqrt
 
-# proj_names='cityair',
-pp = PostProc(dom_names=dom_names)
+def rmse(actual, predicted, skipna=None):
+    l = actual.shape[2]
+    s = 100 * np.isnan(actual).sum(axis=2) / l
+    n = int(.values)
+    return np.sqrt(((actual - predicted) ** 2).mean(axis=2, skipna=skipna))
 
-d = pp.domains.aegean.get_data(slice(0, 640))
+# load time-series data
+ds = xr.open_dataarray('timeseries.nc').load()
+pol_names = ds.coords['pol_name'].values.tolist()
 
-lats, lons = [36.2, 38.4217, 39.0, 37.0], [25.8, 27.1633, 28.0, 29.0]
-# lats, lons = [38.4217, 39.0, 37.0], [27.1633, 28.0, 29.0]
-locs = [Location(lat, lon) for (lat, lon) in zip(lats, lons)]
+actual = ds[:, 0]  # Observations
 
-d2 = pp.domains.aegean.get_data_loc(slice('2015-01-05', '2015-01-10'),
-                                    loc=locs, delta=1, layer_mean=True,
-                                    simplify=False)
+# if NA percent is greater than 25%, exclude data from analysis
+selected = []
+for i in range(ds.shape[0]):
+    sta = ds[i]
+    for j in range(ds.shape[2]):
+        sta2 = sta[:, j]
+        if float(100 * np.isnan(sta2[0]).sum() / len(sta2[0])) < 25:
+            selected.append(ds.isel(sta=i, pol_name=j, drop=True))
+selected = xr.concat(selected, dim='sta')
 
-d2 = pp.domains.aegean.get_data_loc(loc=locs, delta=1, layer_mean=True,
-                                    simplify=False)
+select = [float(100 * np.isnan(p).sum() / len(p)) < 25 for s in ds[:, 0] for p in s]
+
+per = 100 * np.isnan(actual).sum(axis=2) / actual.shape[2]
+actual.where(per > 25)
+n = int(.values)
+
+# plot time series
+for p in pol_names:
+    d = ds.loc[{'pol_name': p}][:, 0:2]
+    new_ds = []
+    for i in range(d.shape[0]):
+        if not np.isnan(d[i, 0]).all().values.tolist():
+            new_ds.append(d[i])
+    d2 = xr.concat(new_ds, dim='sta')
+    n_sta = d2.shape[0]
+    cw = (n_sta, n_sta) if n_sta <= 5 else max_mult(n_sta)
+    g = d2.plot.line(row='sta', hue='project', col_wrap = min(cw),
+                     aspect=2, size=3, sharey=False)
+    g.fig.suptitle(p.upper(), y = 1.0)
+    # plt.tight_layout()
+    file_name = '_'.join(('plot', p)) + '.pdf'
+    g.fig.savefig(file_name, bbox_inches='tight')
+    plt.close(g.fig)
+    print(file_name)
+    # plt.show()
+
+# calculate metrics
+actual = ds[:, 0]
+predicted = ds[:, 1]
+rm = rmse(actual, predicted, False)
+dif = obs - model
 
 
-d3 = pp.domains.aegean.get_data_loc(slice('2015-01-05', '2015-01-10'),
-                                    locs, delta=0, layer_mean=False)
+
+istasyon =
+alper = data.loc[dict(project=[‘obs’,‘cityair’], sta=‘alsancak’,pol_name=‘co’)]
+dod =alper.values
+dod2 = dod.transpose()
+dod2 =dod2[~np.isnan(dod2).any(axis=1)]
+y_actual = dod2[:,0]
+y_predicted = dod2[:,1]*1.18
+rms = sqrt(mp.mean_squared_error(y_actual, y_predicted))
+max_error  = mp.max_error(y_actual,y_predicted)
+r_2  = mp.r2_score(y_actual,y_predicted)
+MAE = mp.mean_absolute_error(y_actual,y_predicted)
+diff = (y_actual-y_predicted)
+MB = diff.mean()
+CO: 1.18*model
+NOX: 1.95*model
+O3: 2.03*model
