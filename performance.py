508f175bf27b1ab17c2682743df2afefa1387a91
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-12-28 04:28:08 +0300

    Update performance.py

--- performance.py
+++ performance.py
@@ -41,6 +41,22 @@ class Metrics:
         return difa.max(axis=axis, skipna=skipna)
 
 
+def split_xarr_by_pols(data, obs, pol_names):
+    # if NA percent is greater than 25%, exclude data from analysis
+    pols = {i: [] for i in pol_names}
+    for i in range(obs.shape[1]):  # loop over pollutants
+        p = obs[:, i]
+        pn = p.coords['pol_name'].values.tolist()
+        for j, s in enumerate(p):
+            na_per = float(100 * np.isnan(s).sum() / len(s))
+            if na_per < 25:
+                o = data.isel(sta=j, pol_name=i, drop=False)
+                o = o.expand_dims(['sta'], [0])
+                pols[pol_names[i]].append(o)
+    return {k: xr.concat(v, dim='sta') for k, v in pols.items()}
+
+
+
 # load time-series data
 ds = xr.open_dataarray('timeseries.nc').load()
 pol_names = ds.coords['pol_name'].values.tolist()
@@ -48,17 +64,7 @@ pol_names = ds.coords['pol_name'].values.tolist()
 actual = ds[:, 0]  # Observations
 
 # if NA percent is greater than 25%, exclude data from analysis
-pols = {i: [] for i in pol_names}
-for i in range(actual.shape[1]):  # loop over pollutants
-    p = actual[:, i]
-    pn = p.coords['pol_name'].values.tolist()
-    for j, s in enumerate(p):
-        na_per = float(100 * np.isnan(s).sum() / len(s))
-        if na_per < 25:
-            o = ds.isel(sta=j, pol_name=i, drop=False)
-            o = o.expand_dims(['sta'], [0])
-            pols[pol_names[i]].append(o)
-pols = {k: xr.concat(v, dim='sta') for k, v in pols.items()}
+pols = split_xarr_by_pols(ds, actual, pol_names)
 
 # plot time series
 for k, v in pols.items():
@@ -74,6 +80,59 @@ for k, v in pols.items():
     print(file_name)
 
 # Calculate Metrics as pandas dataframe
-m = pd.concat([Metrics(v[:, 0], v[:, 1], 1, True).df for k, v in pols.items()])
+metrics = pd.concat([Metrics(v[:, 0], v[:, 1], 1, True).df
+                     for k, v in pols.items()])
+metrics = metrics.rename(columns={'pol_name': 'Pollutant'})
 
+# -----------------------------------------------
+# RELATIVE REDUCTION FACTOR
+ds2 = xr.open_dataarray('reduction.nc').load()
+pol_names = ds2.coords['pol_name'].values.tolist()
 
+obs_2015 = ds2[:, 0]
+r2015 = ds2[:, 1]
+r2025 = ds2[:, 2]
+rwama = ds2[:, 3]
+rwamb = ds2[:, 4]
+
+rrf1 = abs(r2025 - r2015)/r2015
+rrf2 = abs(rwama - r2015)/r2015
+rrf3 = abs(rwamb - r2015)/r2015
+
+obs_2025 = obs_2015 * rrf1
+obs_wama = obs_2015 * rrf2
+obs_wamb = obs_2015 * rrf3
+
+obs_2025 = xr.concat([r2015, obs_2025], dim = 'project')
+obs_2025.coords['project'] = ['cityair', 'cityair_future']
+
+obs_wama = xr.concat([r2015, obs_wama], dim = 'project')
+obs_wama.coords['project'] = ['cityair', 'cityair_wama']
+
+obs_wamb = xr.concat([r2015, obs_wamb], dim = 'project')
+obs_wamb.coords['project'] = ['cityair', 'cityair_wamb']
+
+p2025 = {i: [] for i in pol_names}
+for i in range(obs_2025.shape[2]):  # loop over pollutants
+    p = obs_2025[:, :, i]
+    pn = p.coords['pol_name'].values.tolist()
+    for j in range(p.shape[1]):  # loop over stations
+        s = p[:, j]
+        na_per = float(100 * np.isnan(s[1]).sum() / len(s[1]))
+        if na_per < 25:
+            o = obs_2025.isel(sta=j, pol_name=i, drop=False)
+            o = o.expand_dims(['sta'], [0])
+            p2025[pol_names[i]].append(o)
+p2025 = {k: xr.concat(v, dim='sta') for k, v in p2025.items()}
+
+# plot time series
+for k, v in p2025.items():
+    n_sta = v.shape[0]
+    cw = (n_sta, n_sta) if n_sta <= 5 else max_mult(n_sta)
+    g = v.plot.line(row='sta', hue='project', col_wrap = min(cw),
+                    aspect=2, size=3, sharey=False)
+    g.fig.suptitle(k.upper(), y = 1.0)
+    file_name = '_'.join(('plot_2025', k)) + '.pdf'
+    g.fig.savefig(file_name, bbox_inches='tight')
+    plt.close(g.fig)
+    print(file_name)
