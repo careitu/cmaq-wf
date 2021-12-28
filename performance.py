36498cbf029855b16658d4e0b3b382c26edcd03d
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2021-12-28 03:11:01 +0300

    Update performance.py

--- performance.py
+++ performance.py
@@ -9,6 +9,7 @@ Calculate performance for CMAQ vs. Observations
 """
 import numpy as np
 import xarray as xr
+import pandas as pd
 
 import matplotlib
 import matplotlib.pyplot as plt
@@ -17,18 +18,28 @@ from matplotlib.colors import LinearSegmentedColormap as lscmap
 
 from _helper_functions_ import max_mult
 
-from settings import setting as _set
+class Metrics:
+    def __init__(self, actual, predicted, axis=None, skipna=None):
+        self.rmse = Metrics.rmse(actual, predicted, axis, skipna).to_dataframe('RMSE')
+        self.mae = Metrics.mae(actual, predicted, axis, skipna).to_dataframe('MAE')
+        self.max_err = Metrics.max_error(actual, predicted, axis, skipna).to_dataframe('MAXERR')
+        self.df = pd.concat([self.rmse, self.mae['MAE'], self.max_err['MAXERR']], 1, join='inner')
 
-from DAL import *
+    @classmethod
+    def rmse(cls, actual, predicted, axis=None, skipna=None):
+        dif_sq = (actual - predicted) ** 2
+        return np.sqrt(dif_sq.mean(axis=axis, skipna=skipna))
 
-import sklearn.metrics as mp
-from math import sqrt
+    @classmethod
+    def mae(cls, actual, predicted, axis=None, skipna=None):
+        difa = abs(actual - predicted)
+        return difa.mean(axis=axis, skipna=skipna)
 
-def rmse(actual, predicted, skipna=None):
-    l = actual.shape[2]
-    s = 100 * np.isnan(actual).sum(axis=2) / l
-    n = int(.values)
-    return np.sqrt(((actual - predicted) ** 2).mean(axis=2, skipna=skipna))
+    @classmethod
+    def max_error(cls, actual, predicted, axis=None, skipna=None):
+        difa = abs(actual - predicted)
+        return difa.max(axis=axis, skipna=skipna)
+
 
 # load time-series data
 ds = xr.open_dataarray('timeseries.nc').load()
@@ -37,62 +48,32 @@ pol_names = ds.coords['pol_name'].values.tolist()
 actual = ds[:, 0]  # Observations
 
 # if NA percent is greater than 25%, exclude data from analysis
-selected = []
-for i in range(ds.shape[0]):
-    sta = ds[i]
-    for j in range(ds.shape[2]):
-        sta2 = sta[:, j]
-        if float(100 * np.isnan(sta2[0]).sum() / len(sta2[0])) < 25:
-            selected.append(ds.isel(sta=i, pol_name=j, drop=True))
-selected = xr.concat(selected, dim='sta')
-
-select = [float(100 * np.isnan(p).sum() / len(p)) < 25 for s in ds[:, 0] for p in s]
-
-per = 100 * np.isnan(actual).sum(axis=2) / actual.shape[2]
-actual.where(per > 25)
-n = int(.values)
+pols = {i: [] for i in pol_names}
+for i in range(actual.shape[1]):  # loop over pollutants
+    p = actual[:, i]
+    pn = p.coords['pol_name'].values.tolist()
+    for j, s in enumerate(p):
+        na_per = float(100 * np.isnan(s).sum() / len(s))
+        if na_per < 25:
+            o = ds.isel(sta=j, pol_name=i, drop=False)
+            o = o.expand_dims(['sta'], [0])
+            pols[pol_names[i]].append(o)
+pols = {k: xr.concat(v, dim='sta') for k, v in pols.items()}
 
 # plot time series
-for p in pol_names:
-    d = ds.loc[{'pol_name': p}][:, 0:2]
-    new_ds = []
-    for i in range(d.shape[0]):
-        if not np.isnan(d[i, 0]).all().values.tolist():
-            new_ds.append(d[i])
-    d2 = xr.concat(new_ds, dim='sta')
-    n_sta = d2.shape[0]
+for k, v in pols.items():
+    d = v[:, 0:2]
+    n_sta = d.shape[0]
     cw = (n_sta, n_sta) if n_sta <= 5 else max_mult(n_sta)
-    g = d2.plot.line(row='sta', hue='project', col_wrap = min(cw),
-                     aspect=2, size=3, sharey=False)
-    g.fig.suptitle(p.upper(), y = 1.0)
-    # plt.tight_layout()
-    file_name = '_'.join(('plot', p)) + '.pdf'
+    g = d.plot.line(row='sta', hue='project', col_wrap = min(cw),
+                    aspect=2, size=3, sharey=False)
+    g.fig.suptitle(k.upper(), y = 1.0)
+    file_name = '_'.join(('plot', k)) + '.pdf'
     g.fig.savefig(file_name, bbox_inches='tight')
     plt.close(g.fig)
     print(file_name)
-    # plt.show()
 
-# calculate metrics
-actual = ds[:, 0]
-predicted = ds[:, 1]
-rm = rmse(actual, predicted, False)
-dif = obs - model
+# Calculate Metrics as pandas dataframe
+m = pd.concat([Metrics(v[:, 0], v[:, 1], 1, True).df for k, v in pols.items()])
 
 
-
-istasyon =
-alper = data.loc[dict(project=[‘obs’,‘cityair’], sta=‘alsancak’,pol_name=‘co’)]
-dod =alper.values
-dod2 = dod.transpose()
-dod2 =dod2[~np.isnan(dod2).any(axis=1)]
-y_actual = dod2[:,0]
-y_predicted = dod2[:,1]*1.18
-rms = sqrt(mp.mean_squared_error(y_actual, y_predicted))
-max_error  = mp.max_error(y_actual,y_predicted)
-r_2  = mp.r2_score(y_actual,y_predicted)
-MAE = mp.mean_absolute_error(y_actual,y_predicted)
-diff = (y_actual-y_predicted)
-MB = diff.mean()
-CO: 1.18*model
-NOX: 1.95*model
-O3: 2.03*model
